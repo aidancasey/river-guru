e13ab47d50fbc5d1d85ee5a247cb11befc4f08fe
Aidan Casey <aidancasey@TW-HE-M-AC.local>
2021-01-18 14:51:33 +0000

    change to use tmp folder

--- server/water-flow-rate/dataScraper.js
+++ server/water-flow-rate/dataScraper.js
@@ -60,7 +60,7 @@ async function DownloadLatestPDF() {
   var url = 'http://esbhydro.ie/Lee/04-Inniscarra-Flow.pdf';
   var fullPath = path.resolve('./tmp/');
 
-  var temp_dir = path.join(process.cwd(), 'temp/');
+  var temp_dir = path.join(process.cwd(), 'tmp/');
 if (!fs.existsSync(temp_dir))
 {
     fs.mkdirSync(temp_dir);
