6f53248c7bb4834368a9aa4c8a60a885be471760
Aidan Casey <1399802+aidancasey@users.noreply.github.com>
2021-02-04 22:16:58 +0000

    Add boilerplate vue app and build steps for heroku deploys (#2)
    * add boilerplate for vue app
    
    * adding vue dependencies to package.json on server for heroku deploy to pick up
    
    * adding vue dependencies to package.json on server for heroku deploy to pick up
    
    * adding vue dependencies to package.json on server for heroku deploy to pick up
    
    Co-authored-by: Aidan Casey <aidancasey@tw-he-m-ac.home>

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
 app.use(logger('dev'));
@@ -10,8 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 console.log('in a runnig');
 // Require our routes into the application.
 require('./server/routes')(app);
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
-}));
+
+app.use('/', serveStatic(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/client/dist/index.html')));
 
 module.exports = app;
