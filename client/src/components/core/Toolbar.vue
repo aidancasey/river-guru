278dbea1da700b8fdae111b88e330347880393e1
Aidan Casey <aidancasey@tw-he-m-ac.home>
2021-02-19 22:27:48 +0000

    copy skeleton dashboard components

---
+++ client/src/components/core/Toolbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-toolbar
+    dark
+    app
+    :color="$root.themeColor">
+    <v-toolbar-title> SSSSSS
+      <v-toolbar-side-icon @click="toggleNavigationBar"></v-toolbar-side-icon>
+    </v-toolbar-title>
+  </v-toolbar>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  methods: {
+    toggleNavigationBar() {
+      const vm = this;
+      vm.$emit('toggleNavigationBar');
+    },
+  }
+};
+</script>
+<style>
+  .toolbar-menu-item {
+    padding-left: 5px;
+  }
+
+  .selected-language-flag {
+    max-width: 45px;
+  }
+
+  .language-flag {
+    max-width: 40px;
+  }
+
+
+  .languages-list-item {
+    cursor: pointer;
+    margin-top: -2px;
+    margin-left: 1px;
+  }
+
+  .languages-list-item-title {
+    font-size: 16px;
+  }
+
+  .languages-list-item-title:hover {
+    color: #41B883;
+    font-weight: bold;
+  }
+  .language-menu {
+    border-radius: 25px;
+    width: 235px;
+    margin-right: 10px;
+  }
+</style>
---
+++ client/src/pages/Dashboard.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container fluid grid-list-xl>
+    <v-layout row wrap>
+      <!-- Widgets-->
+      <v-flex d-flex lg3 sm6 xs12>
+        <widget icon="domain" title="1,287,687" :subTitle="$t('widgetHigherYesterday', ['13%'])" :supTitle="$t('widgetTodaysVisit')" color="#00b297"/>
+      </v-flex>
+      <v-flex d-flex lg3 sm6 xs12>
+        <widget icon="money_off" title="$141,291" :subTitle="$t('widgetBeforeTax', ['$117,212'])" :supTitle="$t('widgetTodaysSale')" color="#dc3545"/>
+      </v-flex>
+      <v-flex d-flex lg3 sm6 xs12>
+        <widget icon="computer" title="33.45%" :subTitle="$t('widgetAverageDuration', ['13%'])"  :supTitle="$t('widgetUniqueVisits')" color="#0866C6"/>
+      </v-flex>
+      <v-flex d-flex lg3 sm6 xs12>
+        <widget icon="watch_later" title="13.00%" :subTitle="$t('widgetAverageTime', ['17.25%'])" :supTitle="$t('widgetBounceRate')" color="#1D2939"/>
+      </v-flex>
+      <!-- Widgets Ends -->
+      <!-- Statistics -->
+      <v-flex d-flex lg4 sm6 xs12>
+        <site-view-statistic/>
+      </v-flex>
+      <v-flex d-flex lg4 sm6 xs12>
+        <total-earnings-statistic/>
+      </v-flex>
+      <v-flex d-flex lg4 sm6 xs12>
+        <total-earnings-statistic/>
+      </v-flex>
+      <!-- Statistics Ends -->
+         </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lorem: `Lorem ipsum dolor sit amet, mel at clita quando.`
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
