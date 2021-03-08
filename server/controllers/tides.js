e5f7dfbbc1e7571331971078628732698d33c357
Aidan Casey <aidancasey@TW-HE-M-AC.local>
2021-03-08 22:04:34 +0000

    add eslint config

--- server/controllers/tides.js
+++ server/controllers/tides.js
@@ -1,9 +1,9 @@
-const { index } = require('../tide-times');
+const { GetTideTimes } = require('../tide-times');
 const { DateTime } = require('luxon');
 
 module.exports = {
   GetLatestTides(place,req, res) {
     var today = DateTime.local();
-    index.GetTides(place,today).then((result)=>{res.send(result)});
+    GetTideTimes(place,today).then((result)=>{res.send(result)});
   }
 };
