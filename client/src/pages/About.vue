0872c506fc71482af90144a49c00c7f139974af2
Aidan Casey <aidancasey@tw-he-m-ac.home>
2021-04-01 22:50:38 +0100

    add disclaimer

--- client/src/pages/About.vue
+++ client/src/pages/About.vue
@@ -5,7 +5,7 @@
     <p class="pt-10">
       This website collects river and weather information from different sources
       and presents it in one convenient place for fishermen and anyone
-      interested in the conditions of their local rivers.
+      interested in the conditions of their local rivers around Ireland.
     </p>
 
     <h3 class="display-1">Data Feeds</h3>
@@ -29,11 +29,23 @@
       </a>
     </p>
 
-    <h3 class="display-1">Want To See More Rivers Or Data ?</h3>
+    <h3 class="display-1">Disclaimer</h3>
     <p class="pt-5">
-      If you'd like to see outher rivers from around reported on this site
-      please contact us
+      This site has user generated content. No guarantee can be made for the
+      accuracy of completeness of content. Your use of this content is AT YOUR
+      OWN RISK. River conditions (Such as Gauge Height, Flow Rate,
+      Precipitation, and Temperature) can change without warning, and may be
+      incorrect.
+    </p>
+    <h3 class="display-1">Want To See More Rivers Or More Data ?</h3>
+    <p class="pt-5">
+      If you'd like other irish rivers listed on this site or other data please
+      contact me
       <a href="https://form.jotform.com/210906718558362"> Contact Us</a>
+    </p>
+
+    <p class="pt-5">
+      tight lines folks ! - Aidan
     </p>
   </v-container>
 </template>
