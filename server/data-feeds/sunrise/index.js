77c1f1a700236efcb0309b686b3c0dd0a901c92f
Aidan Casey <aidancasey@tw-he-m-ac.home>
2021-04-06 17:30:49 +0100

    add sequelize model for sun calendar

--- server/data-feeds/sunrise/index.js
+++ server/data-feeds/sunrise/index.js
@@ -1,11 +1,7 @@
 var SunCalc = require("suncalc");
 
 async function GetSunriseDetails(date, latitude, longitude) {
-  // SolarCalc(date,lat,long)
-  // get today's sunlight times for London
   var times = SunCalc.getTimes(new Date(), 51.8974, -8.5688);
-
-  //  var solar = new SolarCalc(new Date("April 6 2021"), 51.8974, -8.5688);
 
   console.log(JSON.stringify(times));
   return times;
