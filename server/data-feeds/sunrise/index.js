41ae0997bbaae59951f3d3b3e78b22f18fa3188a
Aidan Casey <aidancasey@tw-he-m-ac.home>
2021-04-07 13:13:50 +0100

    add some unit tests for sunrise times by river and location

--- server/data-feeds/sunrise/index.js
+++ server/data-feeds/sunrise/index.js
@@ -1,10 +1,40 @@
 var SunCalc = require("suncalc");
+const { RiverLocation } = require("../../models");
 
-async function GetSunriseDetails(date, latitude, longitude) {
-  var times = SunCalc.getTimes(new Date(), 51.8974, -8.5688);
+async function GetSunTimes(date, latitude, longitude) {
+  var details = SunCalc.getTimes(new Date(), latitude, longitude);
 
-  console.log(JSON.stringify(times));
-  return times;
+  console.log("Sunrise is at :" + details.sunrise);
+  console.log("Dawn is at :" + details.dawn);
+  console.log("Sunset is at :" + details.sunset);
+  console.log("Dusk is at :" + details.dusk);
+  return details;
 }
 
-module.exports.GetSunriseDetails = GetSunriseDetails;
+async function GetRiverSunTime(river, locationName) {
+  var location = await RiverLocation.findOne({
+    where: {
+      name: river,
+      location: locationName,
+    },
+  });
+
+  console.log("location details");
+
+  console.log(JSON.stringify(location));
+
+  var details = SunCalc.getTimes(
+    new Date(),
+    location.data.latitude,
+    location.data.longitude
+  );
+
+  console.log("Sunrise is at :" + details.sunrise);
+  console.log("Dawn is at :" + details.dawn);
+  console.log("Sunset is at :" + details.sunset);
+  console.log("Dusk is at :" + details.dusk);
+  return details;
+}
+
+module.exports.GetSunTimes = GetSunTimes;
+module.exports.GetRiverSunTime = GetRiverSunTime;
--- server/test/sunrise.spec.js
+++ server/test/sunrise.spec.js
@@ -4,11 +4,18 @@ const { DateTime } = require("luxon");
 var index = require("../data-feeds/sunrise/index.js");
 
 describe("sunrise-calculator", () => {
-  describe("#GetSunriseDetails()", () => {
-    it("should return a valid URL when passed a valid location and date", () => {
-      const now = DateTime.fromISO(new Date().toISOString());
-      var solarInfo = index.GetSunriseDetails(now, 1, 3);
-      assert.equal(solarInfo != null, true);
+  describe("#GetSunTimes()", () => {
+    it("should return sunrise details when passed valid lat and log co-ordinates", async () => {
+      // const now = DateTime.fromISO(new Date().toISOString());
+      var info = await index.GetSunTimes("foo", 51.8974, -8.5688);
+      assert.equal(info.sunset != null, true);
+    });
+  });
+
+  describe("#GetRiverSunTime()", () => {
+    it("should return a sunrise and sunset times when passed a valid river and location name", async () => {
+      var info = await index.GetRiverSunTime("blackwater", "fermoy");
+      assert.equal(info.sunrise != null, true);
     });
   });
 });
